4b739751a7617bfa77f25ff920f9d6887bd9fced
Mehmet Emin Tiryaki <tiryakieminmehmet@gmail.com>
2025-05-24 21:36:57 +0300

    java 2d array solution

--- java-2d-array/src/Main.java
+++ java-2d-array/src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+/*
+    * Problem: https://www.hackerrank.com/challenges/java-2d-array/problem
+    * maksimum 3. index e kadar ilerleyebilir. sonrasında kum saatinde taşma olur.
+    * toplam 7 elemandan oluşuyor.
+ */
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt).toList()
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        bufferedReader.close();
+
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int sum = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2)
+                        + arr.get(i + 1).get(j + 1)
+                        + arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
+                maxSum = Math.max(maxSum, sum);
+            }
         }
+        System.out.println(maxSum);
     }
 }
