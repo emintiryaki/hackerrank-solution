b5a80bccb08452ae1fa4c096a6b6636424f56a8f
Mehmet Emin Tiryaki <tiryakieminmehmet@gmail.com>
2025-05-24 21:01:22 +0300

    java 1d array solution

---
+++ java-1d-arrray/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int [] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
