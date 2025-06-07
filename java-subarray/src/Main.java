b28f6441c3bcfdd1e0dc161f27e4b6350ba1ee52
Mehmet Emin Tiryaki <tiryakieminmehmet@gmail.com>
2025-06-07 16:28:53 +0300

    java subarray solution

---
+++ java-subarray/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/java-negative-subarray/problem?isFullScreen=true
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
